baa547f5d7b89b50fd8d2e2fa7398813afca84ba
Emily Eisenberg <emily@khanacademy.org>
2012-06-18 18:29:29 -0700

    Add mac complete setup script
    This script should install most everything a new mac needs to develop at Khan.
    It might be missing a few things here and there, but it should at least provide
    a much better start for new people.

---
+++ mac-setup.sh
@@ -0,0 +1,240 @@
+#!/usr/bin/env sh
+
+echo "Running Khan Installation Script 1.0"
+echo "Warning: This is only tested on Mac OS 10.7 (Lion)"
+echo "  After each statement, either something will open for you to"
+echo "    interact with, or a script will run for you to use"
+echo "  Press enter when a download/install is completed to go to"
+echo "    the next step (including this one)"
+
+read
+
+# get user's name/email
+read -p "Enter your KA email for use in git/hg: " email
+read -p "Enter your full name for use on git/hg: " name
+
+echo "Downloading Command Line Tools (log in to start the download)"
+# download the command line tools
+open "https://developer.apple.com/downloads/download.action?path=Developer_Tools/command_line_tools_for_xcode__june_2012/command_line_tools_for_xcode_june_2012.dmg"
+
+read
+
+echo "Running Command Line Tools Installer"
+
+# attach the disk image
+hdiutil attach ~/Downloads/command_line_tools_for_xcode_june_2012.dmg > /dev/null
+echo "Type your password to install:"
+# install the command line tools
+sudo installer -package /Volumes/Command\ Line\ Tools/Command\ Line\ Tools.mpkg -target /
+# detach the disk image
+hdiutil detach /Volumes/Command\ Line\ Tools/ > /dev/null
+
+echo "Opening Hipchat website (log in and click download to install)"
+# open the hipchat page
+open "http://www.hipchat.com/"
+
+read
+
+echo "Installing Homebrew"
+# if homebrew is already installed, don't do it again
+if [ ! -d /usr/local/.git ]
+then
+	/usr/bin/ruby -e "$(/usr/bin/curl -fsSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+fi
+# update brew
+brew update > /dev/null
+
+# make the cellar
+mkdir -p /usr/local/Cellar
+# export some useful directories
+export PATH=/usr/local/bin:/usr/local/sbin:$PATH
+# put these in .bash_profile too
+echo "export PATH=/usr/local/sbin:/usr/local/bin:$$PATH" >> ~/.bash_profile
+
+# brew doctor
+brew doctor
+
+# if no email or name was provided, get it from git
+if [ -n "$email" ]
+then
+	git config --global user.email $email
+else
+	email=`git config --global user.email`
+fi
+if [ -n "$name" ]
+then
+	git config --global user.name "$name"
+else
+	name=`git config --global user.name`
+fi
+
+echo "Installing virtualenv"
+# install pip
+sudo easy_install --quiet pip
+# install virtualenv
+sudo pip install virtualenv -q
+
+echo "Setting up virtualenv"
+
+# make a virtualenv
+virtualenv -q --python=/usr/bin/python2.7 ~/.virtualenv/khan27
+echo "source ~/.virtualenv/khan27/bin/activate" >> ~/.bash_profile
+
+# our function to activate virtualenv
+virtenv() {
+	source ~/.virtualenv/khan27/bin/activate
+}
+
+echo "Installing mercurial"
+# install mercurial
+sudo pip -q install Mercurial
+echo "Installing mercurial in virtualenv"
+virtenv
+# install mercurial in virtualenv
+pip -q install Mercurial
+deactivate
+
+echo "Making khan directory"
+# start building our directory
+mkdir -p ~/khan/
+cd ~/khan/
+
+echo "Cloning stable"
+# get the stable branch
+rm -f ~/.hgrc
+hg clone -q https://khanacademy.kilnhg.com/Code/Website/Group/stable stable 2>/dev/null || (cd stable; hg pull -q -u)
+
+echo "Setting up your .hgrc"
+# make the dummy certificate
+yes "" | openssl req -new -x509 -extensions v3_ca -keyout /dev/null -out dummycert.pem -days 3650 -passout pass:pass 2> /dev/null
+sudo cp dummycert.pem /etc/hg-dummy-cert.pem
+rm dummycert.pem
+# setup the .hgrc
+echo "[ui]
+username = $name <$email>
+
+[extensions]
+rdiff = $HOME/khan/devtools/mercurial-extensions-rdiff/rdiff.py
+kilnauth = $HOME/khan/devtools/kilnauth.py
+hgext.bookmarks =
+color =
+
+[hooks]
+pretxncommit.khanlint = $HOME/khan/devtools/khan-linter/hghook.py
+
+[auth]
+kiln.prefix = https://khanacademy.kilnhg.com
+
+[web]
+cacerts = /etc/hg-dummy-cert.pem" > ~/.hgrc
+
+echo "Installing requirements"
+virtenv
+# install requirements into the virtualenv
+pip -q install -r stable/requirements.txt
+deactivate
+
+echo "Setting up ssh keys"
+
+# if there is no ssh key, make one
+if [ ! -e ~/.ssh/id_rsa ]
+then
+	ssh-keygen -t rsa -C "$email" -f ~/.ssh/id_rsa
+fi
+
+# copy the public key
+cat ~/.ssh/id_rsa.pub | pbcopy
+
+echo "Opening github ssh keys"
+echo "Click 'Add SSH Key', paste into the box, and hit 'Add key'"
+# go to the github ssh keys site
+open "https://github.com/settings/ssh"
+
+read
+
+echo "Cloning exercises (you will probably have to type your ssh key password)"
+# clone exercises
+git clone -q git@github.com:Khan/khan-exercises.git 2>/dev/null || (cd khan-exercises; git pull -q)
+
+echo "Getting development tools"
+# download a bunch of developer tools
+mkdir -p devtools 
+cd devtools
+git clone -q https://github.com/Khan/kiln-review 2>/dev/null || (cd kiln-review; git pull -q)
+hg clone -q https://bitbucket.org/brendan/mercurial-extensions-rdiff 2>/dev/null || (cd mercurial-extensions-rdiff; hg -q pull -u)
+git clone -q https://github.com/Khan/khan-linter 2>/dev/null || (cd khan-linter; git pull -q)
+git clone -q https://github.com/Khan/arcanist 2>/dev/null || (cd arcanist; git pull -q)
+git clone -q https://github.com/Khan/libphutil.git 2>/dev/null || (cd libphutil; git pull -q)
+curl -s https://khanacademy.kilnhg.com/Tools/Downloads/Extensions > /tmp/extensions.zip && unzip -qo /tmp/extensions.zip kiln_extensions/kilnauth.py
+
+cd ~/khan
+
+echo "Installing nginx"
+# install nginx
+brew install nginx
+
+echo "Setting up nginx"
+# setup the nginx configuration file
+echo "worker_processes 1;
+
+events {
+	worker_connections 1024;
+}
+
+http {
+	include			mime.types;
+	default_type		application/octet-stream;
+	sendfile		on;
+	keepalive_timeout	65;
+
+	server {
+		listen		127.0.0.1:80;
+		server_name	stable.ka.local;
+		root		/Users/$USER/khan/stable;
+
+		location / { try_files	$$uri @proxy; }
+		location @proxy { proxy_pass	http://127.0.0.1:8080; }
+	}
+
+	server {
+		listen		127.0.0.1:80;
+		server_name     exercises.ka.local;
+		root		/Users/$USER/khan/khan-exercises;
+		
+		location / {
+			autoindex on;
+			expires epoch;
+		}
+	}
+}" | sudo tee /usr/local/etc/nginx/nginx.conf >/dev/null
+
+# if not done before, add the new hosts to /etc/hosts
+if ! grep -q "ka.local" /etc/hosts
+then
+	echo "# KA local servers
+127.0.0.1       exercises.ka.local
+::1             exercises.ka.local
+127.0.0.1       stable.ka.local
+::1             stable.ka.local" | sudo tee -a /etc/hosts >/dev/null
+fi
+
+# copy the launch plist
+sudo cp /usr/local/Cellar/nginx/*/homebrew.mxcl.nginx.plist /Library/LaunchDaemons
+# delete the username key so it is run as root
+sudo /usr/libexec/PlistBuddy -c "Delete :UserName" /Library/LaunchDaemons/homebrew.mxcl.nginx.plist 2>/dev/null
+# load it
+sudo launchctl load -w /Library/LaunchDaemons/homebrew.mxcl.nginx.plist
+
+echo "Setting up App Engine Launcher"
+curl -s http://googleappengine.googlecode.com/files/GoogleAppEngineLauncher-1.6.6.dmg > ~/Downloads/GoogleAppEngineLauncher-1.6.6.dmg
+hdiutil attach ~/Downloads/GoogleAppEngineLauncher-1.6.6.dmg
+cp -r /Volumes/GoogleAppEngineLauncher-*/GoogleAppEngineLauncher.app /Applications/
+hdiutil detach /Volumes/GoogleAppEngineLauncher-*
+
+echo "Set up the Google App Engine Launcher according to the website."
+open "https://sites.google.com/a/khanacademy.org/forge/for-khan-employees/-new-employees-onboard-doc/developer-setup/launching-your-test-site"
+open -a GoogleAppEngineLauncher
+
+read
+
+echo "You might be done!"
