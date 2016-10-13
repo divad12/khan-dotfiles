2ad501a85275eacf2af07b1f345b55bf0d567cad
Hannah Blumberg <hannah@khanacademy.org>
2016-10-13 12:02:22 -0700

    Create android-emulator.sh.
    Summary:
    This is meant for developers who want to use the app on an emulator (but don't necessarily need to get setup with Android Studio).
    
    Addresses: https://app.asana.com/0/194710008410493/196169730778149
    
    (Context: https://sites.google.com/a/khanacademy.org/forge/for-khan-employees/-new-employees-onboard-doc/developer-setup/mobile-setup/android-setup)
    
    Test Plan:
    - Run script before running android-setup.sh, ensure error message is shown.
    - Run script (after running android-setup.sh), ensure script automatically creates an emulator, builds APK, and installs it.
    - Run script again, ensure script asks if you would like to rebuild the APK, create a new emulator or use an existing one. If you use an existing emulator, ensure it only works with existing device names and asks before reinstalling the APK.
    
    Reviewers: charlie, csilvers, jasonpepas
    
    Reviewed By: charlie, csilvers, jasonpepas
    
    Subscribers: benkomalo, mroth, csilvers, #android
    
    Differential Revision: https://phabricator.khanacademy.org/D31360

---
+++ android-emulator.sh
@@ -0,0 +1,190 @@
+#!/bin/bash
+set -e -o pipefail
+
+# This script helps developers create and run Android emulators as well as build
+# and install the KA APK. As of now, this script can only be run on Mac OSs.
+# (While there is support for Android dev on Linux, none of the mobile
+# developers on the team have tried it.)
+# 
+# This script must be run after mac-android-setup.sh, which installs the JDKs
+# and SDKs and clones the repo necessary to create and run emulators with the KA
+# app. This script can be run whenever you want to create and/or run an Android
+# emulator with the KA app.
+
+ANDROID_REPO="$HOME"/khan/android
+ANDROID_HOME=~/Library/Android/sdk
+
+# Load shared setup functions.
+. "$HOME"/khan/devtools/khan-dotfiles/shared-functions.sh
+
+# Print error in red and exit.
+# $1: error message
+err_and_exit() {
+    printf "\e[0;31m$1\e[0m\n"
+    exit
+}
+
+# Gets input from the user. Return default value if the user gives empty input.
+# $1: prompt
+# $2: default value
+get_input() {
+    read -p "$1" input
+    if [ -z "$input" ]; then
+        echo "$2"
+    else
+        echo "$input"
+    fi
+}
+
+# Ensure $ANDROID_HOME exists; prompt user to run mac-android-setup.sh if not.
+ensure_android_home() {
+    if [ ! -d "$ANDROID_HOME" ]; then
+        err_and_exit "Run ./mac-android-setup.sh, then re-run this script."
+    fi
+}
+
+main() {
+    ensure_android_home
+
+    # Determine if we should build the APK. (Do this before launch_emulator so
+    # that the output doesn't interfere with user input.)
+    should_build_apk=$( get_should_build_apk )
+    if [ "$should_build_apk" = "true" ]; then
+        build_apk
+    fi
+
+    launch_emulator
+
+    install_apk
+}
+
+# Return "true" if there is no available debug APK or if the user chooses to 
+# rebuild it and "false" otherwise.
+get_should_build_apk() {
+    should_build="true"
+    if [ -e "$ANDROID_REPO"/app/build/outputs/apk/app-debug.apk ]; then
+        # If there is a debug APK available, give the user the choice to rebuild
+        # it.
+        input=$( get_yn_input "Would you like to rebuild the APK?" "y" )
+        if [ "$input" = "n" ]; then
+            should_build="false"
+        fi
+    fi
+    echo "$should_build"
+}
+
+# Build debug APK using gradle.
+build_apk() {
+    # TODO(hannah): Display the HEAD hash.
+    update "Building APK..."
+    (
+        cd "$ANDROID_REPO"
+        ./gradlew clean :app:assembleDebug --daemon
+    )
+}
+
+# Launches an existing emulator or creates an emulator and launches it.
+launch_emulator() {
+    if [ -z "$(emulator -list-avds)" ]; then 
+        echo "There are no existing emulators."
+    else
+        echo "Here are existing emulators:"
+        emulator -list-avds
+    fi
+
+    name=$( get_input "Enter the name of the emulator you would like to use, or hit enter to create a new emulator: " "") 
+
+    # Create an emulator if the user hits enter.
+    if [ -z "$name" ]; then
+        update "Creating emulator..."
+        
+        # Get name, API, and ABI from user.
+        echo "The name of your emulator gives you a way to reference it. You might want to name them with the convention device_API_[api]_[abi]."
+        name=$( get_input "Emulator name [test-device]: " "test-device" )
+        echo "API 16 is the oldest API level the KA app supports and API 23 is the newest API."
+        api=$( get_input "API [23]: " "23" )
+        echo "See https://developer.android.com/ndk/guides/abis.html for more information on Android ABIs."
+        abi=$( get_input "ABI [x86_64]: " "x86_64" )
+        
+        ensure_platform_and_abi "$api" "$abi"
+
+        # Say "no" to "Do you wish to create a custom hardware profile".
+        # TODO(hannah): Allow the user to specify the AVD's hardware.
+        echo "no" | android create avd --name "$name" --target android-"$api" --abi google_apis/"$abi"
+    else
+        check_emulator_exists "$name"
+    fi
+
+    # Ensure no emulators are already running.
+    # TODO(hannah): Just use running emulator if available.
+    if [ "$(is_device_ready)" = "true" ]; then
+        err_and_exit "Please close any open emulators."
+    fi
+
+    update "Launching emulator $name..."
+    # Launch emulator with given name and GPU on (see
+    # https://code.google.com/p/android/issues/detail?id=189040).
+    emulator -avd "$name" -gpu on &
+}
+
+# Exit if there is not an emulator with the given name.
+# $1: name of emulator
+check_emulator_exists() {
+    if [ -z "$1" ] || ! emulator -list-avds | grep -w -q "$1" ; then
+        err_and_exit "The emulator $name does not exist. Run 'emulator -list-avds' to see the list of emulators."
+    fi
+}
+
+# Return "true" if the emulator has finished booting up, "false" otherwise. Do
+# not print anything to stdout.
+is_device_ready() {
+    {
+        if [ "`adb shell getprop sys.boot_completed | tr -d '\r' `" = "1" ]; then
+            ready="true"
+        else
+            ready="false"
+        fi
+    } 2> /dev/null
+    echo "$ready"
+}
+
+# Install given platform and ABI if necessary.
+# $1: API
+# $2: ABI
+ensure_platform_and_abi() {
+    if [ ! -d "$ANDROID_HOME/platforms/android-$1" ] ; then
+        update "Installing platform..."
+        # echo y approves the necessary licenses.
+        echo y | android update sdk --no-ui --all --filter android-"$1"
+    fi
+
+    if [ ! -d "$ANDROID_HOME/system-images/android-$1/google_apis/$2" ] ; then
+        update "Installing ABI..."
+        # echo y approves the necessary licenses.
+        echo y | android update sdk --no-ui --all --filter sys-img-"$2"-google_apis-"$1"
+    fi
+}
+
+# Install debug APK on the open emulator.
+install_apk() {
+    # Wait for the emulator to finish booting up.
+    while [ "$(is_device_ready)" = "false" ]; do
+        sleep 0.5
+    done
+    
+    # If the APK is already installed, ask the user if they want to reinstall.
+    if adb shell pm list packages | grep -w -q "org.khanacademy.android.debug" ; then
+        should_install=$( get_yn_input "Would you like to reinstall the APK?" "y" )
+    else
+        should_install="y"
+    fi
+
+    if [ "$should_install" = "y" ]; then
+        update "Installing APK..."
+        # Include -r so we re-install if necessary.
+        adb install -r "$ANDROID_REPO"/app/build/outputs/apk/app-debug.apk
+    fi
+}
+
+# TODO(hannah): Ensure Mac OS.
+main
