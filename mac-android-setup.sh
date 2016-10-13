781aaaed1e3c3621df8a1a8bafc7868d252f3774
Hannah Blumberg <hannah@khanacademy.org>
2016-10-13 12:01:59 -0700

    Create mac-android-setup.sh.
    Summary:
    This script should: intall Java 7 and 8, clone the Android repo, create a symbolic link to codestyle files, set up the Android SDK, and possibly install Android Studio (depending on user input). These steps are outlined here: https://sites.google.com/a/khanacademy.org/forge/for-khan-employees/-new-employees-onboard-doc/developer-setup/mobile-setup.
    
    Addresses: https://app.asana.com/0/194710008410493/194710008410494
    
    (Context: https://sites.google.com/a/khanacademy.org/forge/for-khan-employees/-new-employees-onboard-doc/developer-setup/mobile-setup/android-setup)
    
    Test Plan:
    - Run `./mac-android-setup.sh`. Enter 'n' when prompted about Android Studio.
    - Ensure:
        - Java 7 and 8 are installed (`/usr/libexec/java_home -V`).
        - Android repo is cloned.
        - `~/Library/Android/sdk` exists and contains build-tools, platform-tools, tools.
        - Ensure Android Studio is not installed.
    - Run `./mac-android-setup.sh` again. Enter 'y' when prompted about Android Studio.
        - Ensure Android Studio is installed.
        - Ensure ~/Library/Preferences/AndroidStudio2.2/codestyles/KhanAcademyAndroid.xml exists.
    
    Reviewers: csilvers, jasonpepas
    
    Reviewed By: csilvers, jasonpepas
    
    Subscribers: benkomalo, mroth, #android
    
    Differential Revision: https://phabricator.khanacademy.org/D31359

---
+++ mac-android-setup.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+set -e -o pipefail
+
+# This script sets up developers to work on the Android app and/or test using an
+# Android emulator. As of now, this script can only be run on Mac OSs. (While
+# there is support for Android dev on Linux, none of the mobile developers on
+# the team have tried it.)
+
+# Install in $HOME by default, or an alternate destination specified via $1.
+ROOT=${1-$HOME}
+mkdir -p "$ROOT"
+
+# The directory to which all repositories will be cloned.
+REPOS_DIR="$ROOT/khan"
+
+# Derived path location constants
+DEVTOOLS_DIR="$REPOS_DIR/devtools"
+KACLONE_BIN="$DEVTOOLS_DIR/ka-clone/bin/ka-clone"
+
+ANDROID_HOME=~/Library/Android
+
+# Load shared setup functions.
+. "$DEVTOOLS_DIR"/khan-dotfiles/shared-functions.sh
+
+# Ensure Java 7 and 8 are installed.
+# While Android doesn't support most of the features of Java 8, we use
+# retrolambda to give us the features of Java 8 lambdas. This requires 
+# developers to have JDK 8 installed and set as their JDK.
+install_jdks() {
+    # Ensure some version of Java is installed so /usr/libexec/java_home -V
+    # doesn't cause an error.
+    if ! java -version ; then
+        update "Installing Java 7 SDK..."
+        brew cask install caskroom/versions/java7
+    fi
+
+    # Determine which Java SDKs we have. Note: -V prints to stderr.
+    java_versions=$( /usr/libexec/java_home -V 2>&1 )
+
+    if ! echo "$java_versions" | grep -q -e "Java SE 7"; then
+        update "Installing Java 7 SDK..."
+        brew cask install caskroom/versions/java7
+    fi
+
+    if ! echo "$java_versions" | grep -q -e "Java SE 8"; then
+        update "Installing Java 8 SDK..."
+        brew cask install java
+    fi
+}
+
+# Ensure the Android Github repo is cloned.
+clone_android_repo() {
+    if [ ! -d "$REPOS_DIR"/android ]; then
+        update "Cloning android repository..."
+        kaclone_repo git@github.com:Khan/android "$REPOS_DIR/" -p --email="$gitmail"
+    fi
+}
+
+# Ensure Android Studio is installed (or that the user does not want to install it).
+install_android_studio() {
+    if [ ! -e "/Applications/Android Studio.app" ]; then
+        # If Android Studio is not installed, ask the user before installing it.
+        if [ "$(get_yn_input "Install Android Studio?" "y")" = "y" ]; then
+            update "Installing or updating Android Studio..."
+            brew cask install android-studio
+            configure_codestyle
+        fi
+    fi
+}
+
+# Create a symbolic link to the KA codestyle files so that they can be used by
+# Android Studio.
+configure_codestyle() {
+    mkdir -p ~/Library/Preferences/AndroidStudio2.2/codestyles
+
+    if [ ! -e ~/Library/Preferences/AndroidStudio2.2/codestyles/KhanAcademyAndroid.xml ]; then
+        update "Linking KA codestyle files..."
+        ln -s "$REPOS_DIR"/android/third-party/style-guide/configs/KhanAcademyAndroid.xml ~/Library/Preferences/AndroidStudio2.2/codestyles/
+    fi
+}
+
+# Ensure the Android SDK is installed.
+install_android_sdk() {
+    if [ ! -d "$ANDROID_HOME"/sdk/tools ]; then
+        update "Installing Android SDK..."
+        
+        brew install android-sdk
+        link_sdk
+    fi
+
+    # Update or install SDK components.
+    # Install platform-tools to get adb.
+    echo y | android update sdk --no-ui --all --filter "tools","platform-tools"
+    # Install Build Tools (23.0.2 is necessary to build using gradle).
+    echo y | android update sdk --no-ui --all --filter "build-tools","build-tools-23.0.2"
+    # Install Google Play Services.
+    echo y | android update sdk --no-ui --all --filter "extra-google-google_play_services"
+}
+
+# Make a symbolic link from the default Homebrew location to $ANDROID_HOME.
+link_sdk() {
+    if [ ! -d "$ANDROID_HOME"/sdk/ ]; then
+        # Create the directory to store the Andrid SDK.
+        mkdir -p "$ANDROID_HOME"/sdk
+
+        # Get the directory where Homebrew installed the Android SDK.
+        version_dir=`ls /usr/local/Cellar/android-sdk/ | head -n1`
+
+        ln -s /usr/local/Cellar/android-sdk/"$version_dir"/* "$ANDROID_HOME"/sdk/
+    fi
+}
+
+# TODO(hannah): Ensure setup.sh has already been run, ensure Mac OS.
+install_jdks
+clone_android_repo
+install_android_sdk
+install_android_studio
+
+update "Done! Complete setup instructions at \
+https://sites.google.com/a/khanacademy.org/forge/for-khan-employees/-new-employees-onboard-doc/developer-setup/mobile-setup/android-setup"
--- shared-functions.sh
+++ shared-functions.sh
@@ -1,3 +1,5 @@
+bad_usage_get_yn_input=100
+
 # replacement for clone_repo() function using ka-clone tool for local config
 # if run on an existing repository, will *update* and do --repair
 # $1: url of the repository to clone.  $2: directory to put repo
@@ -22,4 +24,38 @@ kaclone_repo() {
             "$KACLONE_BIN" --repair --quiet "$@"
         fi
     )
+}
+
+# Print update in blue.
+# $1: update message
+update() {
+    printf "\e[0;34m$1\e[0m\n"
+}
+
+# Get yes or no input from the user. Return default value if the user does no
+# enter a valid value (y, yes, n, or no with any captialization).
+# $1: prompt
+# $2: default value
+get_yn_input() {
+    if [ "$2" = "y" ]; then
+        prompt="${1} [Y/n]: "
+    elif [ "$2" = "n" ]; then
+        prompt="${1} [y/N]: "
+    else
+        echo "Error: bad default value given to get_yn_input()" >&2
+        exit $bad_usage_get_yn_input
+    fi
+
+    read -r -p "$prompt" input
+    case "$input" in
+        [yY][eE][sS] | [yY])
+            echo "y"
+            ;;
+        [nN][oO] | [nN])
+            echo "n"
+            ;;
+        *)
+            echo $2
+            ;;
+    esac
 }
