f4e7bf4cf76cf02429fb5713fc29b0db4d47ed83
Ben Kraft <benkraft@khanacademy.org>
2019-04-19 11:36:02 -0700

    Err more clearly if the user doesn't have permissions on webapp
    Summary:
    This is a common error -- and git/github just give us "not found".
    Since we've already cloned several repos, access is really the only
    likely problem, so we can give a much clearer message.
    
    Test Plan: crossed fingers
    
    Reviewers: #devops
    
    Subscribers: csilvers, gerald
    
    Differential Revision: https://phabricator.khanacademy.org/D53644

--- setup.sh
+++ setup.sh
@@ -192,7 +192,13 @@ clone_kaclone() {
 
 clone_webapp() {
     echo "Cloning main webapp repository"
-    kaclone_repo git@github.com:Khan/webapp "$REPOS_DIR/" -p --email="$gitmail"
+    # By this point, we must have git and ka-clone working, so a failure likely
+    # means the user doesn't have access to webapp (it's the only private repo
+    # we clone here) -- we give a more useful error than just "not found".
+    kaclone_repo git@github.com:Khan/webapp "$REPOS_DIR/" -p --email="$gitmail" || add_fatal_error \
+        "Unable to clone Khan/webapp -- perhaps you don't have access? " \
+        "If you can't view https://github.com/Khan/webapp, ask #it in " \
+        "Slack to be added."
 }
 
 # clones a specific devtool
