cd3359bbff09a3cfebbba83217f60af07452e025
Sean Driedger-Bauer <sean@khanacademy.org>
2021-01-19 11:39:52 -0600

    Fix khan-dotfiles app-install step to use new brew syntax
    Summary:
    When running khan-dotfiles, the `install_mac_apps` step currently fails
    to install any apps. Manually running `brew cask install <whatever>`
    reveals this error message:
    `Error: Calling brew cask install is disabled! Use brew install [--cask]
    instead.`
    
    In this diff I tweak the script to follow that direction.
    
    Test Plan:
    * Manually test by running `make`, proceeding through to the
      `install_mac_apps` step, and seeing that apps can be installed
    
    Reviewers: #devops, ebrown, boris
    
    Reviewed By: #devops, ebrown, boris
    
    Subscribers: boris, ebrown, csilvers
    
    Differential Revision: https://phabricator.khanacademy.org/D68604

--- mac-setup.sh
+++ mac-setup.sh
@@ -546,7 +546,7 @@ install_mac_apps() {
   for app in ${chosen_apps[@]}; do
     if ! brew cask ls $app >/dev/null 2>&1; then
         info "$app is not installed, installing $app"
-        brew cask install $app || warn "Failed to install $app, perhaps it is already installed."
+        brew install --cask $app || warn "Failed to install $app, perhaps it is already installed."
     else
         success "$app already installed"
     fi
