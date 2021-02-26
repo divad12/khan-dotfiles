48ed87c6986a527e4a929d0d9ee8a0f147eaeecf
Eric (iCloud) Brown <ericbrown@khanacademy.org>
2021-02-25 18:12:03 -0800

    Make python2 installation more robust
    Summary:
    While khan-dotfiles are supposed to be idempotent - allowing
    re-running 'make' several times both to pickup where it left
    off and to "fix" things that might have broken, there are a
    lot of corner cases to test in these scripts.
    
    Changes here:
    * Properly 'reinstall' python@2 with --force
    * Verify the correct version of pip2 is installed
      (whether python2 is already installed or not)
    
    Issue: https://khanacademy.atlassian.net/browse/INFRA-6002
    
    Test Plan:
    * Ensure khan-dotfiles 'make' works
    * Ensure it fixes pip2
    * Ensure it is a no-op when everything is just fine
    * Ensure --force works properly
    
    Reviewers: #devops, boris
    
    Reviewed By: #devops, boris
    
    Subscribers: boris, csilvers
    
    Differential Revision: https://phabricator.khanacademy.org/D69644

--- bin/install-mac-python2.py
+++ bin/install-mac-python2.py
@@ -2,8 +2,8 @@
 """Install Khan's python2."""
 
 import argparse
+import re
 import subprocess
-import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--force", help="Force install of Khan's python2",
@@ -11,10 +11,28 @@ parser.add_argument("--force", help="Force install of Khan's python2",
 args = parser.parse_args()
 
 which = subprocess.run(['which', 'python2'], capture_output=True, text=True)
-if which.stdout.strip() != "/usr/bin/python2":
+is_installed = which.stdout.strip() != "/usr/bin/python2"
+if is_installed:
     print("Already running a non-system python2.")
-    if not args.force:
-        sys.exit(0)
 
-print("Installing python2 from khan/repo. This may take a few minutes.")
-subprocess.run(['brew', 'install', 'khan/repo/python@2'], check=True)
+if args.force or not is_installed:
+    action = "reinstall" if is_installed else "install"
+    print("Installing python2 from khan/repo. This may take a few minutes.")
+    subprocess.run(['brew', action, 'khan/repo/python@2'], check=True)
+
+# Get version of pip2
+pip2_version = ""
+pip2_version_str = subprocess.run(['pip2', '--version'],
+                                  capture_output=True, text=True)
+if pip2_version_str:
+    match = re.match(r'\w+ (\d+)', pip2_version_str.stdout)
+    if match:
+        pip2_version = match.group(1)
+
+if pip2_version and pip2_version > "19":
+    print("Reverting pip2 from version: " + pip2_version_str.stdout.strip())
+    subprocess.run(['pip2', 'install', 'pip2<20', '-U'], check=True)
+
+# Simple diagnostics
+subprocess.run(['pip2', '--version'])
+print("which python2: " + which.stdout.strip())
